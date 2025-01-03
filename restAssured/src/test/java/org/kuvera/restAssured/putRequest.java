3136bff0c38185ca0694c835c8bf187f0aa5151c
rashmijg <rashmiskp222@gmail.com>
2025-01-03 21:52:41 +0530

    first commit

---
+++ restAssured/src/test/java/org/kuvera/restAssured/putRequest.java
@@ -0,0 +1,20 @@
+package org.kuvera.restAssured;
+
+import org.testng.annotations.Test;
+
+public class putRequest {
+
+	// hashmap
+	 // org.json
+	//pojo classes
+	//extenal jason
+	
+	@Test
+	public void hashmap()
+	{
+		Hashmap data = new hashmap(); 
+		        data.
+	}
+	
+	
+}
